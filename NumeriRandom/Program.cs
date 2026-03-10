0d60e6e5bae6e0bcec3c105f8c7daffa456db9f3
Alessia Tonicello <alessiatonicello@gmail.com>
2026-03-10 16:50:45 +0100

    esercizi random

---
+++ NumeriRandom/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace NumeriRandom
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {   /*
+            Random random = new Random(); //Random = sintassi, random = per richiamarlo
+            //int numero = random.Next(1, 10);
+            //Console.WriteLine(numero);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int numero1 = random.Next(1, 11);  // da 1 a 91 per il gioco della tombola
+                Console.WriteLine(numero1);
+            }*/
+            
+            
+            //Esercizio 1: simulazione di un dado
+            //Creare un programma che simuli il lancio di un dado. Il programma deve:
+            //- generare un numero casuale tra 1 e 6 
+            //- stampare il risultato del lancio
+            //BONUS: possibilità do lanciare anche più dadi con più facce
+            
+            Random random = new Random(); 
+            int numero = random.Next(1, 7); 
+            Console.WriteLine("Lanciando il dado è uscito il numero " + numero);
+            
+            Console.WriteLine("Quanti dadi vuoi lanciare?");
+            int quantitaDadi = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Quante facce hanno i dadi? (es. 6, 12, 20)");
+            int numeroFacce = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nLancio di {quantitaDadi} dadi a {numeroFacce} facce:");
+
+            int sommaTotale = 0;
+
+            for (int i = 1; i <= quantitaDadi; i++)
+            {
+                int risultato = random.Next(1, numeroFacce + 1);
+    
+                Console.WriteLine($"Dado {i}: {risultato}");
+                sommaTotale += risultato;
+            }
+            
+            Console.WriteLine("La somma totale dei lanci è: " + sommaTotale);
+            
+            //Esercizio 2 - indovina il numero (while true finché non indovini il numero)
+            //Il computer genera un numero casuale tra 1 e 50. L'utente deve indovinare il numero. Il prog deve dire:
+            //- troppo alto 
+            //- troppo basso
+            //- hai indovinato
+            
+            
+            Random random2 = new Random(); 
+            int numero2 = random2.Next(1, 51);
+            int tentativo; 
+            
+            Console.WriteLine("Prova ad indovinare");
+
+            while (true)
+            {
+                Console.Write("Inserisci il tuo numero: ");
+                tentativo = Convert.ToInt32(Console.ReadLine());
+
+                if (tentativo < numero2)
+                {
+                    Console.Write("Troppo basso!");
+                }
+                else if (tentativo > numero2)
+                {
+                    Console.Write("Troppo alto!");
+                }
+                else
+                {
+                    Console.Write("Bravo, hai indovinato!");
+                    break;
+                }
+            }
+            Console.WriteLine(" Fine del gioco!");
+        } 
+    }
+}
---
+++ doWhile/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace doWhile
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            int i = 1;
+            do
+            {
+                Console.WriteLine("Numero " + i);
+                i++;
+            } while (i <= 10);
+
+
+            int numero;
+            while (true)
+            {
+                Console.WriteLine("Inserisci un numero (0 per uscire)");
+                numero =  int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    Console.WriteLine("Uscita in corso");
+                    break;
+                }
+                Console.WriteLine("Hai inserito questo numero: " + numero);
+            }
+
+
+            //Esercizio 1: chiedere all'utente una password finché non inserisce quella corretta
+            string password;
+            string passwordCorretta = "Segreta123!";
+
+            do
+            {
+                Console.WriteLine("Inserisci la password: ");
+                password = Console.ReadLine();
+
+                if (password != passwordCorretta)
+                {
+                    Console.WriteLine("La password è errata, riprova");
+
+                }
+            } while (password != passwordCorretta);
+
+            Console.WriteLine("Accesso garantito!");
+
+
+
+            //Esercizio 2: creare un programma che chieda un numero finché non è compreso tra 1 e 10
+            int numero;
+
+            do
+            {
+                Console.WriteLine("Inserisci un numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+            } while (numero >= 1 && numero <= 10);
+
+            Console.WriteLine("Accesso garantito!");
+            
+        
+
+
+            //Esercizio 3: creare un menu ripetuto con do while
+            int scelta;
+
+            do
+            {
+                Console.WriteLine("1. Profilo");
+                Console.WriteLine("2. Impostazioni");
+                Console.WriteLine("3. Contatti");
+                Console.WriteLine("4. Esci");
+                Console.Write("Scegli un'opzione: ");
+
+                scelta = Convert.ToInt32(Console.ReadLine());
+
+                switch (scelta)
+                {
+                    case 1:
+                        Console.WriteLine("Personalizza profilo");
+                        break;
+                    case 2:
+                        Console.WriteLine("Cambia nome utente, email, password");
+                        break;
+                    case 3:
+                        Console.WriteLine("email, LinkedIn, numero di telefono");
+                        break;
+                    case 4:
+                        Console.WriteLine("Logged out");
+                        break;
+                }
+
+            } while (scelta != 4); 
+            Console.WriteLine("Salvataggio in corso... Arrivederci!");
+            */
+            
+            //Esercizio 3: chiedere all'utente numeri e contare quanti numeri sono stati inseriti.
+            //Il programma termina quando viene inserito 0.
+            int contatore = 0;
+            int numeroInserito;
+
+            do
+            {
+                Console.WriteLine("Inserisci un numero");
+                numeroInserito = Convert.ToInt32(Console.ReadLine());
+                
+                contatore++;
+                
+            } while (numeroInserito != 0);
+            Console.WriteLine("Hai inserito in totale " + contatore + " numeri");
+            
+        }
+
+    }
+}
